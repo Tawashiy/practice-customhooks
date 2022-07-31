212675cd8e0a989c524c44888c5593efc9d4b4d9
Tawashiy <97021226+Tawashiy@users.noreply.github.com>
2022-07-31 15:40:30 +0900

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-
-const user = {
-  id: 1,
-  name: "おなまえさん",
-  email: "おなまえさん",
-  address: "おなまえさん"
-};
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
